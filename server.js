f2f741cca62eb0b9cf1012f4ca2755b551825284
Lahari-5139 <vishnu_lahari@yahoo.com>
2021-11-27 15:33:02 +0530

    added socket.io in server code

--- server.js
+++ server.js
@@ -4,6 +4,80 @@ require('./config/db');
 const app = require('express')();
 const port = 3000;
 
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200 
+}
+const http = require('http').createServer(app);
+// const mongoose = require('mongoose');
+const socketio = require('socket.io')
+const io = socketio(http,{
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+}); 
+const { addUser, getUser, removeUser } = require('./utils/chat');
+const Message = require('./models/message');
+const PORT = process.env.PORT || 5000;
+const Room = require('./models/room');
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    Room.find().then(result => {
+        socket.emit('output-rooms', result)
+    })
+    socket.on('create-room', name => {
+        const room = new Room({ name });
+        room.save().then(result => {
+            io.emit('room-created', result)
+        })
+    })
+    socket.on('join', ({ name, room_id, user_id }) => {
+        const { error, user } = addUser({
+            socket_id: socket.id,
+            name,
+            room_id,
+            user_id
+        })
+        socket.join(room_id);
+        if (error) {
+            console.log('join error', error)
+        } else {
+            console.log('join user', user)
+        }
+    })
+    socket.on('sendMessage', (message, room_id, callback) => {
+        const user = getUser(socket.id);
+        const msgToStore = {
+            name: user.name,
+            user_id: user.user_id,
+            room_id,
+            text: message
+        }
+        console.log('message', msgToStore)
+        const msg = new Message(msgToStore);
+        msg.save().then(result => {
+            io.to(room_id).emit('message', result);
+            callback()
+        })
+
+    })
+    socket.on('get-messages-history', room_id => {
+        Message.find({ room_id }).then(result => {
+            socket.emit('output-messages', result)
+        })
+    })
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+    })
+});
+
+
+
 const UserRouter = require('./api/user');
 const MasterRouter = require('./api/master');
 const PasswordRouter = require('./api/passwordReset');
@@ -24,6 +98,8 @@ const SearchRouter = require('./api/search');
 // for accepting post form data
 const bodyParser = require('express').json;
 app.use(bodyParser());
+app.use(cors(corsOptions));
+app.use(cookieParser()); 
 
 // app.use('/user', UserRouter)
 
